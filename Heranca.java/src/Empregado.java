059f8654ea0725f44be67b09b86642417c150468
Yasmin345 <Yasminmonteiros29@gmail.com>
2024-05-18 21:44:57 -0300

    [feat] - adicionando construtores

--- Heranca.java/src/Empregado.java
+++ Heranca.java/src/Empregado.java
@@ -3,4 +3,20 @@ public class Empregado {
     protected String nome;
     protected String endereço;
     protected double salario; 
+
+    // construtor 
+    public Empregado(String nome, String endereco, double salario){
+        this.nome = nome;
+        this.endereço = endereco;
+        this.salario = salario;
+
+        
+    }
+
+    // construtor vazio
+    public Empregado(){
+
+    }
+
+    
 }
