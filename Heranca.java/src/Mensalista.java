ce201b2c5f71c41c8a32f06bd1d2e6b85759b162
Yasmin345 <Yasminmonteiros29@gmail.com>
2024-05-18 23:18:11 -0300

    [feat] - adicionando método de calculo

--- Heranca.java/src/Mensalista.java
+++ Heranca.java/src/Mensalista.java
@@ -20,6 +20,18 @@ public class Mensalista extends Empregado {
         this.cargo = cargo;
     }
 
+    public void calcularSalario (){
+        if (cargo.equals("Junior")){
+            salario = 2500;
+
+        }else if (cargo.equals("Pleno")){
+            salario = 3500;
+
+        }else {
+            salario = 5500;
+        }
+    }
+
     
 
     
